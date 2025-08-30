f9dc814860ad1dae0adf8d4eb2e30aa4f435e64e
Wyatt Lansdale <wyatt.lansdale@gmail.com>
2025-08-30 12:25:06 -0400

    Add Head component to RootLayout for setting page title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,9 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-reanimated';
+import Head from 'expo-router/head';
 
 import { useColorScheme } from '@/components/useColorScheme';
 
@@ -51,6 +52,9 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Head>
+        <title>ping</title>
+      </Head>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
